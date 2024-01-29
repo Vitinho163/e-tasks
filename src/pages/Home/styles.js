77550ee4e9c65a6063c1910870d08745c204cc28
João Victor <victor.jvsb18@gmail.com>
2024-01-29 16:51:54 -0300

    Aplicando media queries na Home

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-
+import { DEVICE_BREAKPOINTS } from "../../styles/deviceBreakpoints";
 export const Container = styled.div`
   width: 100%;
   height: 100vh;
@@ -10,6 +10,15 @@ export const Container = styled.div`
   grid-template-areas: 
     "menu top"
     "menu content";
+
+  @media (max-width: ${DEVICE_BREAKPOINTS.MD}) {
+    grid-template-columns: auto;
+    grid-template-rows: auto 1fr;
+
+    grid-template-areas: 
+    "top"
+    "content";
+  }
 `;
 
 export const FixedContent = styled.section`
